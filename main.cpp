f379c5f37cb8af8499a986526250a81def2709ee
Lyndon Panton <LyndonPanton@gmail.com>
2025-01-01 19:00:12 +0000

    Player can manually exit the game

--- main.cpp
+++ main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <vector>
 
-void printOptions(std::vector<std::string>&);
-void setOptions(std::vector<std::string>&);
+void clear_record();
+void exit_game();
+void play_game();
+void print_introduction(); // Done
+void print_options(std::vector<std::string>&); // Done
+void print_record();
+void set_options(std::vector<std::string>&); // Done
 
 int main(int argc, char* argv[])
 {
+    std::cout << std::endl;
+
     bool playing = true;
     std::vector<std::string> options;
-    setOptions(options);
+    set_options(options);
 
     while (playing)
     {
-        std::cout << std::endl;
-        std::cout << "Welcome to Rock, Paper, Scissors game!" << std::endl;
-        std::cout << std::endl;
 
         std::cout << "What would you like to do?" << std::endl;
         std::cout << std::endl;
 
+        int option;
         bool validOption = false;
 
         while (!validOption)
         {
-            printOptions(options);
-
-            int option;
+            print_options(options);
 
             std::cin >> option;
 
@@ -39,20 +42,77 @@ int main(int argc, char* argv[])
             std::cout << "Invalid option entered..." << std::endl << std::endl;
         }
 
-        break;
+        switch (option)
+        {
+            case 1:
+                play_game();
+                break;
+            case 2:
+                print_record();
+                break;
+            case 3:
+                playing = false;
+                exit_game();
+                break;
+        }
     }
+
+    std::cout << std::endl;
 
     return 0;
 }
 
-void setOptions(std::vector<std::string>& options)
+void clear_record()
 {
-    options.push_back("Play (vs. AI)");
-    options.push_back("Check record");
-    options.push_back("Exit");
+    // Check record file exists
+
+    // Reset values to 0 if it does
+    std::cout << std::endl << "No record found..." << std::endl;
 }
 
-void printOptions(std::vector<std::string>& options)
+void exit_game()
+{
+    // Save any data necessary...
+
+    std::cout << std::endl << "Thank you for playing!";
+}
+
+void play_game()
+{
+    std::cout << "What are you using? (R, P, or S)" << std::endl;
+
+
+    char option;
+    bool validOption = false;
+
+    while (!validOption)
+    {
+        std::cin >> option;
+
+        switch (option)
+        {
+            case 'R':
+            case 'P':
+            case 'S':
+                validOption = true;
+        }
+    }
+
+    std::cout << "Rock, Paper, Scissors...!" << std::endl;
+    std::cout << "{ player option } " << "(Player) vs. " << " {ai option}";
+    std::cout << " (AI)" << std::endl << std::endl;
+
+    std::cout << " {winner} " << " wins!" << std::endl;
+}
+
+void print_introduction()
+{
+    std::cout << std::endl;
+    std::cout << "Welcome to Rock, Paper, Scissors game!" << std::endl;
+    std::cout << std::endl;
+}
+
+void print_options(std::vector<std::string>& options)
 {
     int i = 1;
 
@@ -63,4 +123,26 @@ void printOptions(std::vector<std::string>& options)
 
     std::cout << std::endl << "Enter a number (" << "1 - " << options.size();
     std::cout << "): ";
+}
+
+void print_record()
+{
+    std::cout << std::endl << "No record found..." << std::endl;
+
+    /*
+        Create a record file with the following format:
+
+            { Player wins } 
+            { AI wins }
+    */
+
+   // Check if record file exists
+
+}
+
+void set_options(std::vector<std::string>& options)
+{
+    options.push_back("Play (vs. AI)");
+    options.push_back("Check record");
+    options.push_back("Exit");
 }
